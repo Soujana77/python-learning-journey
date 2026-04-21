6419bdae08fa1613ad4b873aeda7b90b5e2011ce
Soujanya Jain <varshajanya77@gmail.com>
2026-04-21 22:32:00 +0530

    Learnt about rules for variables and operators

---
+++ chapter-2/03_rules_variables.py
@@ -0,0 +1,10 @@
+a = 7 
+hari = 77
+_hari = 777
+hari_1 = 7777
+Hari = 77777
+hari_ = 777777
+
+#@hari = 7 is inavlid due to @ symbol
+#1hari = 7 is invalid due to starting with a number
+#hari-1 = 7 is invalid due to - symbol
---
+++ chapter-2/04_operators.py
@@ -0,0 +1,41 @@
+#Airthemetic Operators
+a = 10
+b = 3           
+print(a+b) #Addition
+
+#Assignment Operators
+a = 4-2 #Assign 4-2 in a 
+print(a)
+b = 6
+# b += 3 #Increment the value of b by 3 and then assign it to b 
+b -= 2 #Decrement the value of b by 2 and then assign it to b
+print(b)
+
+#Comaprison Operators
+d = 5<4
+print(d)
+
+e = 5==5
+print(e)
+
+f = 5!=5
+print(f)
+
+#Logical Operators
+g = True and False
+print(g)
+
+#Truth Table for or operator
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or False is", False or False)
+print("False or True is", False or True)
+
+#Truth Table for and operator
+print("True and False is", True and False)
+print("True and True is", True and True)
+print("False and False is", False and False)
+print("False and True is", False and True)
+
+print(not True) #not operator negates the value of True to False
+print(not False) #not operator negates the value of False to True
