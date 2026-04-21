013d6bc25c5236202c2e4d70d14aa4f5102210d6
Soujanya Jain <varshajanya77@gmail.com>
2026-04-21 22:43:05 +0530

    Learnt about typecasting and inputs

---
+++ chapter-2/05_type.py
@@ -0,0 +1,11 @@
+a = 31
+t = type(a)
+print(t) #class 'int'
+
+b = float(a) #typecasting a to float and assigning it to b
+print(b) #31.0
+
+c = str(a) #typecasting a to string and assigning it to c
+print(c) #31
+print(type(c)) #class 'str'
+
---
+++ chapter-2/06_input.py
@@ -0,0 +1,15 @@
+a = input("Enter number 1")
+
+b = input("Enter number 2")
+
+print("Number a is ", a)
+print("Number b is ", b)
+print("Sum of a and b is ", a+b) #this will concatenate the two strings instead of adding them as numbers
+
+a = int(input("Enter number 1"))
+
+b = int(input("Enter number 2"))
+
+print("Number a is ", a)
+print("Number b is ", b)
+print("Sum of a and b is ", a+b) #this will add the two numbers as integers
