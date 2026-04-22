5de3dafc928f4627e8060fa5b59b6ea356612157
Soujanya Jain <varshajanya77@gmail.com>
2026-04-22 23:01:45 +0530

    Started wit strings

---
+++ chapter-3/01_intro_to_strings.py
@@ -0,0 +1,14 @@
+a = "souj"
+
+b = 'souj'
+
+c = '''souj'''
+#three declarations of string
+
+#string slicing
+name = "soujanya"
+nameshort = name[0:4] #slicing the string from index 0 to 3 (4 is exclusive)
+print(nameshort)
+
+character1 = name[0] #slicing the string to get the first character
+print(character1)
---
+++ chapter-3/02_negative_slicing.py
@@ -0,0 +1,7 @@
+name = "Supriya"
+
+print(name[0:3])
+
+print(name[-4:-1]) #negative slicing, it starts from the end of the string and goes backwards, -1 is exclusive
+print(name[:4]) # is same as print(name[0:4])
+print(name[1:4])# is same as print(name[1:len(name)]) or print(name[1:4]) because it will go till the end of the string
