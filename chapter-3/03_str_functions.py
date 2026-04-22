ce9ef2d453794c1b288e84e1134a56c40850ab81
Soujanya Jain <varshajanya77@gmail.com>
2026-04-22 23:14:50 +0530

    Learnt about string functions and escape charaters

---
+++ chapter-3/03_str_functions.py
@@ -0,0 +1,8 @@
+a = "Soujanya"
+
+print(len(a))
+print(a.endswith("a")) #returns True if the string ends with "a"
+print(a.startswith("p")) #returns True if the string starts with "p"
+print(a.capitalize())
+print(a.upper())
+print(a.lower())        
---
+++ chapter-3/04_escape_sequence.py
@@ -0,0 +1,6 @@
+a = "Sanvi is a good girl\nbut not a bad \"girl\""
+print(a)
+b = "This is a backslash \\"
+print(b)
+c = "This is a tab\tand this is a new line\nThis is a new line"
+print(c)        
