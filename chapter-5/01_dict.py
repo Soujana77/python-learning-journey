56d17608988b6a5811e09fa5395ddc7497dc6532
Soujanya Jain <varshajanya77@gmail.com>
2026-04-24 21:20:47 +0530

    Started with dictonarires and dictionary methods

---
+++ chapter-5/01_dict.py
@@ -0,0 +1,11 @@
+marks = {
+    "soujj":79,
+    "sanjana": 89,
+    "sneha": 90
+}
+
+print(marks)
+print(marks["soujj"])
+marks["soujj"] = 80 #dictionaries are mutable, it means that we can change the value of a dictionary after it is created.
+print(marks)
+print(marks["soujj"])   
---
+++ chapter-5/02_dict_methods.py
@@ -0,0 +1,17 @@
+marks = {
+    "Sanvi": 95,
+    "Rohan": 85,    
+    "Akash": 90,
+    "0" : "Harry"
+}
+
+print(marks.items())
+#items() is a dictionary method that returns a view object that displays a list of a dictionary's key-value tuple pairs.
+print(marks.keys())
+#keys() is a dictionary method that returns a view object that displays a list of all the keys in the dictionary.
+print(marks.values())
+#values() is a dictionary method that returns a view object that displays a list of all the values in the dictionary.
+marks.update({"Rohan":99 , "Sanvi":70}) #update() is a dictionary method that updates the dictionary with the key-value pairs from another dictionary or from an iterable of key-value pairs. If the key already exists in the dictionary, its value will be updated with the new value.
+print(marks)    
+marks.get("Rohan")#get() is a dictionary method that returns the value of the specified key. If the key does not exist in the dictionary, it returns None (or a specified default value).
+print(marks.get("Rohan"))
