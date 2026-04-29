defba50b66967edda5cb1a7693313c09b4a4e481
Soujanya Jain <varshajanya77@gmail.com>
2026-04-29 22:32:04 +0530

    Learnt about write function

---
+++ chapter-9/02_file_write.py
@@ -0,0 +1,6 @@
+st = "Sanvi is a good girl , wvery enthusiastic girl "
+
+f = open("chapter-9/myfile.txt", "w") # specify folder name to create folder in desired path when youw want to write into a new file 
+
+f.write(st)
+f.close()
