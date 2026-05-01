e5d17b133a6bdbfd0bba5b3f410d559213ae4c38
Soujanya Jain <varshajanya77@gmail.com>
2026-05-01 14:13:15 +0530

    Solved problem- 5,6 from PS

---
+++ chapter-9/chapter 9-PS/problem-5.py
@@ -0,0 +1,14 @@
+# Question 5:
+# Repeat problem 4 for a list of such words to be censored.
+
+words = ["Donkey", "bad", "ugly"]
+
+with open("file.txt", "r") as f:
+    content = f.read()
+
+for word in words:
+    content = content.replace(word, "#" * len(word))
+
+with open("file.txt", "w") as f:
+    f.write(content)
+    
