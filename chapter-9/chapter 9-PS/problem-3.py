3edea7eddc8fcd9f4065e7d630d010968e5a051b
Soujanya Jain <varshajanya77@gmail.com>
2026-05-01 14:10:31 +0530

    Solved problem-3, 4 from PS

---
+++ chapter-9/chapter 9-PS/problem-3.py
@@ -0,0 +1,8 @@
+# Question 3:
+# Write a program to generate multiplication tables from 2 to 20
+# and write it to different files.
+
+for i in range(2, 21):
+    with open(f"tables/table_{i}.txt", "w") as f:
+        for j in range(1, 11):
+            f.write(f"{i} x {j} = {i*j}\n")
