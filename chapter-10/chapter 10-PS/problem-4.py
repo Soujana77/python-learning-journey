6fd7f16e3394f6902784eab14a4a542e55fef3f5
Soujanya Jain <varshajanya77@gmail.com>
2026-05-03 22:32:12 +0530

    Solved problem 3, 4 from PS

---
+++ chapter-10/chapter 10-PS/problem-4.py
@@ -0,0 +1,20 @@
+# add  a static method in problem 2 to greet the user attribute
+class Calculator:
+        def __init__(self, n):
+            self.n = n
+
+            def square(self):
+                print(f"The square is {self.n*self.n}")
+                def square(self):
+                    print(f"The square is {self.n*self.n}")
+                    def cube(self):
+                        print(f"The cube is {self.n*self.n*self.n}")
+                        def square_root(self):
+                            print(f"The square root is {self.n**0.5}")
+
+@staticmethod
+def greet():
+    print("Good Morning")
+a = Calculator(4)
+a.square()
+a.greet()
