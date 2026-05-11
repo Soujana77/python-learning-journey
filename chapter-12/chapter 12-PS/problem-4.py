de1bcc054d5ff5fbebd530687c42fab7ea3708ba
Soujanya Jain <varshajanya77@gmail.com>
2026-05-12 00:01:42 +0530

    Solved problem 4 :

---
+++ chapter-12/chapter 12-PS/problem-4.py
@@ -0,0 +1,12 @@
+# Question 4:
+# Write a program to display a/b where a and b are integers.
+# If b = 0, display infinite by handling ZeroDivisionError.
+
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    print("Result:", a / b)
+
+except ZeroDivisionError:
+    print("Infinite")
